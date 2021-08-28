05a103394eaee49eb5c248374ff3e8a009405b50
Umer Aqeel <31266008+umeraqeel786@users.noreply.github.com>
2021-08-28 18:52:47 +0500

    Update BaseOperation.sol

--- BaseOperation.sol
+++ BaseOperation.sol
@@ -1,39 +1,119 @@
 pragma solidity >=0.7.0 <0.9.0;
 
-
 import "@openzeppelin/contracts-ethereum-package/contracts/access/Ownable.sol";
 
-contract BaseOperations  is  Ownable {
+contract BaseOperations  is Ownable   {
  
  
- address private _owner;
-
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-   
-    constructor() {
-        _setOwner(_msgSender());
-    }
- function owner() public view virtual returns (address) {
-        return _owner;
-    }
+ 
+ address private owner;
+    uint256 a;
+    uint256 b;
+    uint256 c;
+    uint256 firstNumber;
+    uint256 SecondNumber;
+    uint256 Ans;
     modifier onlyOwner() {
-        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+     require(msg.sender == owner, "ERROR This is not an Owner");
         _;
     }
-
-    function renounceOwnership() public virtual onlyOwner {
-        _setOwner(address(0));
-    }
-    function transferOwnership(address newOwner) public virtual onlyOwner {
-        require(newOwner != address(0), "Ownable: new owner is the zero address");
-        _setOwner(newOwner);
+  
+    constructor() {
+        owner = msg.sender; 
     }
 
-    function _setOwner(address newOwner) private {
-        address oldOwner = _owner;
-        _owner = newOwner;
-        emit OwnershipTransferred(oldOwner, newOwner);
+ struct Address {
+       
+        address tokenAddress;
     }
+
+//Add
+    
+    function Add(uint256 a, uint256 b) public onlyOwner view returns (uint256) {
+      
+       return a +b;
+    }
+
+ 
+ 
+ //Divide
+
+    
+    function Divide(uint256 firstNumber , uint256 SecondNumber) public onlyOwner view returns(uint Ans, uint remainder){
+     require(firstNumber > SecondNumber, "ERROR");
+      Ans  = firstNumber / SecondNumber;
+        remainder = firstNumber - SecondNumber * Ans;
+   }
+   
+   
+   //Multiply 
+     function mul(uint256 firstNumber , uint256 SecondNumber) internal returns (uint256){
+          for (uint j=1; j<=  SecondNumber;  j ++) {  
+          Ans += firstNumber;        
+      }
+      return Ans;
+     }
+     
+     
+     function Multiply(uint256 firstNumber, uint256 SecondNumber) public onlyOwner  returns (uint256) {
+     mul( firstNumber ,  SecondNumber);
+     
+     }
+    
+   
+  
+  function Check_Mul_Ans() public view returns(uint256){
+      return Ans;
+
+  }
+  
+  
+  // getPercent
+  
+   function getPercent(uint firstNumber, uint SecondNumber) public pure returns(uint percent) {
+        uint denominator = SecondNumber * 1000;
+        require(denominator > SecondNumber);  
+        uint temp = denominator / firstNumber + 5; 
+        return temp / 10;
+    }
+
+// addAddresses
+
+    mapping(bytes32 => Address[]) tokenSymbol;
+   
+
+    function addAddresses(bytes32 _name, address _tokenAddress) public {
+        Address memory myTokenAddress = Address(_tokenAddress);
+        tokenSymbol[_name].push(myTokenAddress);
+    }
+
+    
+    function getAddress(bytes32 _name)  public view   returns (address[] memory addresss)
+    {
+        uint256 length = tokenSymbol[_name].length;
+        addresss = new address[](length);
+
+        for (uint256 i = 0; i < length; i++) {
+            addresss[i] = tokenSymbol[_name][i].tokenAddress;
+        }
+    }
+
+   
+    function stringToBytes32(string memory source)
+        public
+        pure
+        returns (bytes32 result)
+    {
+        bytes memory tempEmptyStringTest = bytes(source);
+        if (tempEmptyStringTest.length == 0) {
+            return 0x0;
+        }
+
+        assembly {
+            result := mload(add(source, 32))
+        }
+    }
+    
+
 
 }
