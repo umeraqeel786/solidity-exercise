1086185df8be3a758608f2ac5b418d967ea60197
Umer Aqeel <31266008+umeraqeel786@users.noreply.github.com>
2021-08-28 11:08:27 +0500

    Create Multiply.sol

---
+++ Multiply.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+
+contract MultiplyNumber {
+
+    address private owner;
+    uint256 firstNumber;
+    uint256 SecondNumber;
+    uint256 Ans;
+  
+    
+    modifier onlyOwner() {
+     require(msg.sender == owner, "ERROR This is not an Owner");
+        _;
+    }
+  
+    constructor() {
+        owner = msg.sender; 
+    }
+
+
+
+     function mul(uint256 firstNumber , uint256 SecondNumber) internal returns (uint256){
+          for (uint j=1; j<=  SecondNumber;  j ++) {  
+          Ans += firstNumber;        
+      }
+      return Ans;
+     }
+     
+     
+     function Multiply(uint256 firstNumber, uint256 SecondNumber) public onlyOwner returns(uint256) {
+     mul( firstNumber ,  SecondNumber);
+     }
+    
+   
+
+  
+  function Check_Ans() public view returns(uint256){
+      return Ans;
+  }
+ 
+}
